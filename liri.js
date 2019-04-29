d64007677943f2e256521e5fe62816cef85979fd
Patience D <patience.deaton2@outlook.com>
2019-04-29 18:31:58 -0400

    fixed undefined issue, changed movie if/else statement

--- liri.js
+++ liri.js
@@ -13,7 +13,7 @@ let spotify = new Spotify(keys.spotify);
 
 let userInput = process.argv.slice(3).join("+");
 
-if (process.argv[2] === "spotify-this-song" && process.argv[3] === "undefined") {
+if (process.argv[2] === "spotify-this-song" && process.argv[3] === undefined) {
     spotify.search({ type: 'track', query: "the+sign" }).then(data => {
         console.log("Artist Name: " + data.tracks.items[3].artists[0].name);
         console.log("Album Name: " + data.tracks.items[3].album.name);
@@ -25,52 +25,46 @@ if (process.argv[2] === "spotify-this-song" && process.argv[3] === "undefined") 
         });
 
 } else if (process.argv[2] === "spotify-this-song") {
-    music(userInput);
+    spotify.search({ type: 'track', query: userInput }).then(function (data) {
+        console.log("Artist Name: " + data.tracks.items[0].artists[0].name);
+        console.log("Song Name: " + data.tracks.items[0].name);
+        console.log("Where to Listen: " + data.tracks.items[0].external_urls.spotify);
+        console.log("Album Name: " + data.tracks.items[0].album.name);
+    })
+        .catch(err => {
+            console.log("This is the error: " + err);
+        });
 
-    function music() {
-        spotify.search({ type: 'track', query: userInput }).then(function (data) {
-            console.log("Artist Name: " + data.tracks.items[0].artists[0].name);
-            console.log("Song Name: " + data.tracks.items[0].name);
-            console.log("Where to Listen: " + data.tracks.items[0].external_urls.spotify);
-            console.log("Album Name: " + data.tracks.items[0].album.name);
-        })
-            .catch(err => {
-                console.log("This is the error: " + err);
-            });
-    }
+} else if (process.argv[2] === "movie-this" && process.argv[3] === undefined) {
+    console.log("Title: Mr. Nobody");
+    console.log("Year: 2013");
+    console.log("imdb Rating: 7.8");
+    console.log("Rotten Tomatoes Rating: undefined");
+    console.log("Country Filmed in: Belgium, Germany, Canada, France, USA, UK");
+    console.log("Language: English, Mohawk");
+    console.log("The Plot: A boy stands on a station platform as a train is about to leave. Should he go with his mother or stay with his father? Infinite possibilities arise from this decision. As long as he doesn't choose, anything is possible.");
+    console.log("The Actors: Jared Leto, Sarah Polley, Diane Kruger, Linh Dan Pham");
+
 
 } else if (process.argv[2] === "movie-this") {
-
     let movie = process.argv[3];
     let queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
-
     console.log(queryUrl);
 
     axios.get(queryUrl).then(response => {
-        if (movie) {
-            // console.log(response)
-            console.log("Title: " + response.data.Title);
-            console.log("Year: " + response.data.Year);
-            console.log("imdb Rating: " + response.data.imdbRating);
-            console.log("Rotten Tomatoes Rating: " + response.data.Ratings.Value);
-            console.log("Country Filmed in: " + response.data.Country);
-            console.log("Language: " + response.data.Language);
-            console.log("The Plot: " + response.data.Plot);
-            console.log("The Actors: " + response.data.Actors);
-        } else {
-            console.log("Title: Mr. Nobody");
-            console.log("Year: 2013");
-            console.log("imdb Rating: 7.8");
-            console.log("Rotten Tomatoes Rating: undefined");
-            console.log("Country Filmed in: Belgium, Germany, Canada, France, USA, UK");
-            console.log("Language: English, Mohawk");
-            console.log("The Plot: A boy stands on a station platform as a train is about to leave. Should he go with his mother or stay with his father? Infinite possibilities arise from this decision. As long as he doesn't choose, anything is possible.");
-            console.log("The Actors: Jared Leto, Sarah Polley, Diane Kruger, Linh Dan Pham");
-        }
+        console.log("Title: " + response.data.Title);
+        console.log("Year: " + response.data.Year);
+        console.log("imdb Rating: " + response.data.imdbRating);
+        console.log("Rotten Tomatoes Rating: " + response.data.Ratings.Value);
+        console.log("Country Filmed in: " + response.data.Country);
+        console.log("Language: " + response.data.Language);
+        console.log("The Plot: " + response.data.Plot);
+        console.log("The Actors: " + response.data.Actors);
     })
         .catch(error => {
             console.log("This is the error: " + error)
         });
+
 
 } else if (process.argv[2] === "concert-this") {
     let artist = process.argv[3];
@@ -78,7 +72,7 @@ if (process.argv[2] === "spotify-this-song" && process.argv[3] === "undefined") 
     console.log(queryUrl);
 
     axios.get(queryUrl).then(response => {
-        console.log(response.data.Venue);
+        console.log(response.data);
     })
 }
 
